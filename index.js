26ab33334038100870f86278564c33b9953200a0
ViavaCos <13811092694@163.com>
2021-06-04 08:33:43 +0800

    update

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const { app, BrowserWindow, dialog, Menu, MenuItem, Notification, Tray }  = require('electron')
+const { app, BrowserWindow, ipcMain, dialog, Menu, MenuItem, Notification, Tray }  = require('electron')
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 400,
-    height: 600,
-    // transparent: true // 透明边框
-    // webPreferences: { // 开启网页功能设置
-    //   nodeIntegration: true
-    // },
-    // resizable: false,
+    width: 401,
+    height: 601,
+    transparent: true, // 透明边框
+    webPreferences: { // 开启网页功能设置
+      nodeIntegration: true, // 是否集成 Nodejs
+      preload: path.join(__dirname, 'preload.js')
+    },
+    resizable: false,
+    // useContentSize: true,
     backgroundColor: '#00c297', // 背景颜色
-    opacity: 0.8, // 窗体透明度 0~1
-    icon: './icon.png' // 应用图标
-    // frame: false // 设置无边框窗口（无工具栏、边框、其它图形化外壳）
+    opacity: 1, // 窗体透明度 0~1
+    icon: './icon.png', // 应用图标
+    frame: false // 设置无边框窗口（无工具栏、边框、其它图形化外壳）
+  })
+
+  // 监听关闭事件
+  ipcMain.on('close-app', () => {
+    if (win) {
+      win.close()
+    }
   })
 
   // 创建应用图标
-  const appIcon = new Tray('./icon.png')
+  // const appIcon = new Tray('./icon.png')
 
   // 设置无工具栏
   Menu.setApplicationMenu(null)
 
-  // win.loadFile('./build/index.html')
-  win.loadFile('index.html')
+  win.loadFile('./build/index.html')
+  // win.loadFile('index.html')
 
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('ping', { text: 'This is a text.'})
-  })
+  // win.webContents.on('did-finish-load', () => {
+  //   win.webContents.send('ping', { text: 'This is a text.'})
+  // })
 
-  win.webContents.on('mineclick', () => {
-    console.log('Click MEssage received.!');
-  })
+  // win.webContents.on('mineclick', () => {
+  //   console.log('Click MEssage received.!');
+  // })
 }
 
 // notification
@@ -90,8 +100,8 @@ app.on('activate', () => {
 // }))
 // Menu.setApplicationMenu(menu)
 
-function test () {
-  console.log('test');
-}
+// function test () {
+//   console.log('test');
+// }
 
-exports.test = test
+// exports.test = test
